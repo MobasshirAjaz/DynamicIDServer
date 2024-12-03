d3746a469b4fb2cf1bdfea6807aea064b66fc12b
Mobasshir Ajaz <mobasshirajaz@gmail.com>
2024-12-03 13:22:06 +0530

    Made improvements to encrypt.js and added functionality

--- encrypt.js
+++ encrypt.js
@@ -1,6 +1,13 @@
 const cryptojs = require("crypto-js");
 
-function encrypt(obj) {
+function caesarcipher(text, key) {
+  return text
+    .split("")
+    .map((char) => String.fromCharCode(char.charCodeAt(0) + key))
+    .join("");
+}
+
+function encryptQR(obj) {
   let str = "";
 
   for (const key in obj) {
@@ -19,6 +26,26 @@ function encrypt(obj) {
   console.log(combined);
   return combined;
 }
+
+function decryptQR(qrcontent) {
+  qrcontent = qrcontent.toString();
+  const [hexIv, hexCiphertext] = qrcontent.split("#");
+
+  const iv = CryptoJS.enc.Hex.parse(hexIv);
+  const ciphertext = CryptoJS.enc.Hex.parse(hexCiphertext);
+
+  const key = CryptoJS.enc.Utf8.parse("mysecretaeskey16");
+  const decrypted = CryptoJS.AES.decrypt({ ciphertext: ciphertext }, key, {
+    iv: iv,
+  });
+
+  const decryptedStr = decrypted.toString(CryptoJS.enc.Utf8);
+  console.log(decryptedStr);
+  return decryptedStr;
+}
 const testObj = { name: "Mobasshir", roll: 22052727, year: 4 };
-encrypt(testObj);
-exports.encrypt;
+
+//encryptQR(testObj);
+let encrypted = caesarcipher("13:16:48", 5);
+console.log(encrypted);
+console.log(caesarcipher(encrypted, -5));
